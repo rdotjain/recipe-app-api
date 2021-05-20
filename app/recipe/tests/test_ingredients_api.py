915f29598907e91e38fe9c951e0ae86b17793263
rdotjain <rupanshijain45678@gmail.com>
2021-05-21 01:18:20 +0530

    Added create ingredient feature

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -48,8 +48,8 @@ class PrivateIngredientsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
-    def ingredients_limited_to_user(self):
-        """Test taht ingredients only for authenticated user are returned"""
+    def test_ingredients_limited_to_user(self):
+        """Test that ingredients only for authenticated user are returned"""
         user2 = get_user_model().objects.create_user(
             'test2@xyz.com',
             'testpass'
@@ -62,3 +62,25 @@ class PrivateIngredientsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test create a new ingredient"""
+        payload = {
+            'name': 'Cabbage'
+        }
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {
+            'name': ''
+        }
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
